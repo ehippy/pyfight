f74ac0b2f7a90656aea72e9ad6f0a469365ad46e
Patrick McDavid <pmcdavid@gmail.com>
2017-08-22 22:18:50 -0600

    support login

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from chalice import Chalice
+import os
+import urllib.request
+import json
+import jwt
+
+from chalice import Chalice, Response
 from chalicelib import *
 
 app = Chalice(app_name='chill')
@@ -9,16 +14,44 @@ def index():
     return {'hello': 'world', 'state': GAME_STATE_NEW}
 
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
+@app.route('/hello/{name}')
+def hello_name(name):
+    return {'hello': name}
+
+
+@app.route('/auth')
+def auth():
+    auth_code = app.current_request.query_params['code']
+    uri = 'https://slack.com/api/oauth.access?client_id=' + os.environ['SLACK_CLIENT_ID'] + '&client_secret='\
+          + os.environ['SLACK_SECRET'] + '&code=' + auth_code
+    response = urllib.request.urlopen(uri).read()
+    json_response = json.loads(response)
+
+    if json_response['ok']:
+
+        cookie_payload = {
+            "user_id": json_response['user']['id'],
+            "user_name": json_response['user']['name'],
+            "user_img": json_response['user']['image_192'],
+            "team_id": json_response['team']['id'],
+            "team_domain": json_response['team']['domain'],
+            "team_img": json_response['team']['image_230'],
+        }
+
+        encoded_cookie = jwt.encode(cookie_payload, os.environ['JWT_SECRET'], algorithm='HS256').decode("utf-8")
+        redir_url = "http://localhost:4200/?cookie=%s" % encoded_cookie
+
+        return Response(
+        status_code=301,
+        body='',
+        headers={'Location': redir_url})
+
+    print(json)
+
+    return {
+        "dict": app.current_request.to_dict()
+    }
+
 # @app.route('/users', methods=['POST'])
 # def create_user():
 #     # This is the JSON body the user sent in their POST request.
