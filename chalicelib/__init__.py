85698379c3ce3a5c59f039d2e5906829346b3e43
Patrick McDavid <pmcdavid@gmail.com>
2017-08-28 23:08:19 -0600

    dodge around postgres?

--- chalicelib/__init__.py
+++ chalicelib/__init__.py
@@ -17,11 +17,14 @@ ACTION_TYPE_MOVE = 1
 ACTION_TYPE_ATTACK = 2
 ACTION_TYPE_EXPAND_RANGE = 3
 
-# db = SqliteExtDatabase('my_database.db')
 
-db = PostgresqlDatabase('postgres',
-                        host='localhost', port=32768, user='postgres', password='postgres',
-                        autocommit=True, autorollback=True)
+try:
+    db = PostgresqlDatabase('postgres',
+                            host='localhost', port=32768, user='postgres', password='postgres',
+                            autocommit=True, autorollback=True)
+except Exception:
+    print("Could not connect to postgres, going with sqlite")
+    db = SqliteExtDatabase('my_database.db')
 
 
 class BaseModel(Model):
